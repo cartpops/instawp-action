39fa88a022dbb6ff3572cdb2f3d1a9f979d8ffe7
Jaeden <ch.schwartze@gmail.com>
2024-04-25 15:21:37 +0200

    chore: add build

--- dist/index.js
+++ dist/index.js
@@ -29028,88 +29028,107 @@ const utils_1 = __nccwpck_require__(442);
  */
 async function run() {
     try {
-        const { GITHUB_TOKEN, INSTAWP_TOKEN, INSTAWP_ACTION, INSTAWP_TEMPLATE_SLUG, REPO_ID, ARTIFACT_URL } = (0, utils_1.getWorkflowInput)();
+        const { GITHUB_TOKEN, INSTAWP_TOKEN, INSTAWP_ACTION, INSTAWP_TEMPLATE_SLUG, REPO_ID, ARTIFACT_URL, TIMEOUT_SECONDS } = (0, utils_1.getWorkflowInput)();
         if (!constants_1.INSTAWP_ACTIONS.includes(INSTAWP_ACTION)) {
             core.setFailed(`Invalid action: ${INSTAWP_ACTION}. Must be one of: ${constants_1.INSTAWP_ACTIONS.join(', ')}`);
+            return;
         }
         const octokit = (0, github_1.getOctokit)(GITHUB_TOKEN);
         const pullRequestsNo = github_1.context.payload.pull_request?.number ?? 0;
         const repo = github_1.context.repo;
         if (INSTAWP_ACTION === 'create-site-template-git') {
             core.info(`Creating site template with slug: ${INSTAWP_TEMPLATE_SLUG}`);
+            if (ARTIFACT_URL) {
+                core.info(`Using artifact URL: ${ARTIFACT_URL}`);
+            }
             const config = {
                 template_slug: INSTAWP_TEMPLATE_SLUG,
-                site_name: github_1.context.repo.repo,
+                site_name: `${repo.owner}-${repo.repo}-pr-${pullRequestsNo}`,
                 pr_num: pullRequestsNo,
                 repo_id: REPO_ID,
                 override_url: ARTIFACT_URL ?? undefined
             };
+            const response = await (0, utils_1.createSiteGit)(config, { token: INSTAWP_TOKEN });
+            if (!response?.data) {
+                core.setFailed(`Failed to create site template: ${response?.message ?? 'No data returned from InstaWP API'}`);
+                return;
+            }
+            core.info(`Site template created at: ${response.data.wp_url}`);
+            const taskId = response.data.task_id;
+            const timeout = new Promise((_, reject) => setTimeout(() => reject(new Error('Task timeout reached, InstaWP API did not respond')), TIMEOUT_SECONDS));
             try {
-                const response = await (0, utils_1.createSiteGit)(config, {
-                    token: INSTAWP_TOKEN
-                });
-                if (!response?.data) {
-                    const msg = response?.message ?? 'No data returned from InstaWP API';
-                    core.setFailed(`Failed to create site template: ${msg}`);
-                }
-                core.info(`Site template created at: ${response.data.wp_url}`);
-                const taskId = response.data.task_id;
-                let taskInProgress = true;
-                while (taskInProgress) {
-                    const taskStatus = await (0, utils_1.getTaskStatus)(taskId, {
-                        token: INSTAWP_TOKEN
-                    });
-                    if (taskStatus.data.status !== 'progress') {
-                        core.info(`Site created at: ${response.data.wp_url}`);
-                        break;
-                    }
-                    taskInProgress = true;
-                    core.info(`Waiting for site creation...`);
-                    await new Promise(resolve => setTimeout(resolve, 2000));
-                }
-                const wpUrl = response.data.wp_url;
-                const wpMagicLoginLink = (0, utils_1.getMagicLink)(response.data.s_hash);
-                core.setOutput('instawp_url', wpUrl);
-                core.setOutput('instawp_magic_login_url', wpMagicLoginLink);
-                if (pullRequestsNo > 0) {
-                    const comments = await octokit.rest.issues.listComments({
-                        ...repo,
-                        issue_number: pullRequestsNo,
-                        per_page: 100
-                    });
-                    const comment = comments.data.find(c => c?.body?.includes('<!-- INSTAWP-COMMENT -->'));
-                    if (undefined === comment) {
-                        await octokit.rest.issues.createComment({
-                            ...repo,
-                            issue_number: pullRequestsNo,
-                            body: `<!-- INSTAWP-COMMENT -->\nWordPress Instance Deployed.\n\nURL: [${wpUrl}](${wpUrl})\nMagic Login: [${wpMagicLoginLink}](${wpMagicLoginLink})`
-                        });
-                    }
-                    else {
-                        await octokit.rest.issues.updateComment({
-                            ...repo,
-                            issue_number: pullRequestsNo,
-                            comment_id: comment.id,
-                            body: `<!-- INSTAWP-COMMENT -->\nWordPress Instance Deployed.\n\nURL: [${wpUrl}](${wpUrl})\nMagic Login: [${wpMagicLoginLink}](${wpMagicLoginLink})`
-                        });
-                    }
-                }
+                await Promise.race([
+                    (async function checkStatus() {
+                        let status = 'progress';
+                        while (status === 'progress') {
+                            const taskStatus = await (0, utils_1.getTaskStatus)(taskId, {
+                                token: INSTAWP_TOKEN
+                            });
+                            status = taskStatus.data.status;
+                            if (status !== 'progress') {
+                                core.info(`Site created at: ${response.data.wp_url}`);
+                                break;
+                            }
+                            core.info('Waiting for site creation...');
+                            await new Promise(resolve => setTimeout(resolve, 2000));
+                        }
+                    })(),
+                    timeout
+                ]);
             }
             catch (error) {
-                const err = error;
-                core.setFailed(err.message);
+                const message = error instanceof Error ? error.message : 'Task timeout reached';
+                core.setFailed(message);
+                return;
             }
+            const wpUrl = response.data.wp_url;
+            const wpMagicLoginLink = (0, utils_1.getMagicLink)(response.data.s_hash);
+            core.setOutput('instawp_url', wpUrl);
+            core.setOutput('instawp_magic_login_url', wpMagicLoginLink);
+            await updateOrCreateComment({
+                octokit,
+                repo,
+                pullRequestsNo,
+                wpUrl,
+                wpMagicLoginLink
+            });
         }
-        else
-            [core.setFailed(`${INSTAWP_ACTION} has not been implemented yet.`)];
+        else {
+            core.setFailed(`${INSTAWP_ACTION} has not been implemented yet.`);
+        }
     }
     catch (error) {
-        // Fail the workflow run if an error occurs
         if (error instanceof Error)
             core.setFailed(error.message);
     }
 }
 exports.run = run;
+async function updateOrCreateComment({ octokit, repo, pullRequestsNo, wpUrl, wpMagicLoginLink }) {
+    if (pullRequestsNo > 0) {
+        const comments = await octokit.rest.issues.listComments({
+            ...repo,
+            issue_number: pullRequestsNo,
+            per_page: 100
+        });
+        const comment = comments.data.find(c => c?.body?.includes('<!-- INSTAWP-COMMENT -->'));
+        const body = `<!-- INSTAWP-COMMENT -->\nWordPress Instance Deployed.\n\nURL: [${wpUrl}](${wpUrl})\nMagic Login: [${wpMagicLoginLink}](${wpMagicLoginLink})`;
+        if (undefined === comment) {
+            await octokit.rest.issues.createComment({
+                ...repo,
+                issue_number: pullRequestsNo,
+                body
+            });
+        }
+        else {
+            await octokit.rest.issues.updateComment({
+                ...repo,
+                issue_number: pullRequestsNo,
+                comment_id: comment.id,
+                body
+            });
+        }
+    }
+}
 
 
 /***/ }),
@@ -29135,6 +29154,7 @@ const getWorkflowInput = () => {
     const INSTAWP_TOKEN = (0, core_1.getInput)('instawp-token');
     const INSTAWP_ACTION = (0, core_1.getInput)('instawp-action');
     const INSTAWP_TEMPLATE_SLUG = (0, core_1.getInput)('instawp-template-slug');
+    const TIMEOUT_SECONDS = (0, core_1.getInput)('timeout-seconds', { required: false });
     const ARTIFACT_URL = (0, core_1.getInput)('instawp-artifact-zip-url', {
         required: false
     }) ?? null;
@@ -29144,7 +29164,8 @@ const getWorkflowInput = () => {
         INSTAWP_ACTION,
         INSTAWP_TEMPLATE_SLUG,
         ARTIFACT_URL,
-        REPO_ID
+        REPO_ID,
+        TIMEOUT_SECONDS: TIMEOUT_SECONDS ? parseInt(TIMEOUT_SECONDS) : 120
     };
 };
 exports.getWorkflowInput = getWorkflowInput;
